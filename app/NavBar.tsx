fa022c806f0bf710bb080cd5f0585c9994d97379
@wobote <william.obote@mediapal.net>
2023-10-29 18:44:40 +0300

    Build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import {AiFillBug } from 'react-icons/ai';
+
+const NavBar  = () => {
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/"> <AiFillBug /> </Link>
+        <ul className='flex space-x-6'>
+            <li><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href="/">Dashboard</Link></li>
+            <li><Link href="/issues">Issues</Link></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
