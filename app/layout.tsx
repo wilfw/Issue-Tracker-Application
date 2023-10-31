09f402804e9a0ec5fe02d476e451e914c7420e25
@wobote <william.obote@mediapal.net>
2023-10-31 18:20:07 +0300

    Customized Radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import '@radix-ui/themes/styles.css';
+import './theme.config.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import NavBar from './NavBar'
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,9 +25,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-
-        <Theme>
+      <body className={inter.variable}>
+      <Theme appearance="light" accentColor="purple">
         
         <NavBar />
         <main className='p-5'>{children}</main>
